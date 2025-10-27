8dec3f0074f1588d6689bf147cd40f4f38228d0d
GoldenVers <152583222+GoldenVers@users.noreply.github.com>
2025-10-27 13:22:06 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.listen(port, () => {
 
 //connect to database
 
-mongoose.connect('mongodb+srv://goldenyoussarts:youssef24@cluster0.t6dukqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('')
 .then(() => {
     console.log('Connected to database');
     })
